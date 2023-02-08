95bed811ca8f4eb5af9751f44e1e9eddbbcaa229
Alexandre Lima <alexandreatlima@gmail.com>
2023-02-08 10:52:52 -0300

    aula axios 2

--- md02/rev-routes-e-axios/src/pages/Home/index.jsx
+++ md02/rev-routes-e-axios/src/pages/Home/index.jsx
@@ -1,13 +1,33 @@
 import axios from "axios";
 import { Link } from "react-router-dom";
-import movies from "../../movies";
+// import movies from "../../movies";
+import { useEffect, useState } from "react";
 
 export function Home() {
-  const apiResponse = axios.get(
-    "https://api.themoviedb.org/3/discover/movie?api_key=1dbc566a4812e099606bf66f83159d6e"
-  );
+  const [movies, setMovies] = useState([]);
+  console.log("Oi");
 
-  console.log(apiResponse);
+  useEffect(() => {
+    async function fetchMovies() {
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/discover/movie?api_key=1dbc566a4812e099606bf66f83159d6e"
+      );
+
+      setMovies(response.data.results);
+    }
+
+    fetchMovies();
+  }, []);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(
+  //       "https://api.themoviedb.org/3/discover/movie?api_key=1dbc566a4812e099606bf66f83159d6e"
+  //     )
+  //     .then((delivery) => {
+  //       setMovies(delivery.data.results);
+  //     });
+  // }, []);
 
   return (
     <>
--- md02/rev-routes-e-axios/src/pages/MoviePage/index.jsx
+++ md02/rev-routes-e-axios/src/pages/MoviePage/index.jsx
@@ -1,12 +1,30 @@
 import { useParams } from "react-router-dom";
-import movies from "../../movies";
+// import movies from "../../movies";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export function MoviePage() {
   const params = useParams();
+  const [movie, setMovie] = useState({});
 
-  const movie = movies.filter((currentMovie) => {
-    return currentMovie.id === Number(params.movieId);
-  })[0];
+  // const movie = movies.filter((currentMovie) => {
+  //   return currentMovie.id === Number(params.movieId);
+  // })[0];
+
+  useEffect(() => {
+    async function fetchMovie() {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${params.movieId}?api_key=1dbc566a4812e099606bf66f83159d6e`
+      );
+
+      console.log(response);
+
+      setMovie(response.data);
+    }
+
+    fetchMovie();
+  }, []);
 
   return (
     <>
